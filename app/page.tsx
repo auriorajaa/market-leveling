a41e49577fad8d2e8f491c07e0f1aeb2d39768e5
Aurio Rajaa <riorajaa2018@gmail.com>
2026-05-31 11:11:03 +0700

    change theme color

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive">
-        Hello World
-      </Button>
+    <div className="p-8 max-w-2xl mx-auto space-y-10">
+      <h1>Hello</h1>
     </div>
   );
 }
